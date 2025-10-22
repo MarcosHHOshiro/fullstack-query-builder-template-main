15891677b89510469a7942816d1de5af33757cb7
Marcos H. Hisano Oshiro <marcos.2h.oshiro@gmail.com>
2025-10-22 14:52:39 -0400

    Creating table migration

---
+++ knexfile.ts
@@ -0,0 +1,13 @@
+import { connect } from "http2";
+
+export default {
+    client: 'sqlite3',
+    connection: {
+        filename: "./src/database/database.db"
+    },
+    useNullAsDefault: true,
+    migrations: {
+        extension: 'ts',
+        directory: './src/database/migrations'
+    }
+}
